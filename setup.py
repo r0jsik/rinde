72ce7b91fa12abcfa0c1fdbcd62ecc3d12ab12ac
r0jsik <31357349+r0jsik@users.noreply.github.com>
2019-02-11 17:14:10 +0100

    Minor improvements

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from setuptools import setup, find_packages
 setup(
 	name="rinde",
 	description="Library used for creating GUI based on XML and CSS.",
-	version="0.82",
+	version="0.82-A",
 	license="MIT License",
 	author="Marcin Rajs",
-	
-	requires=["pygame", "cssutils"],
+	requires=("pygame", "cssutils"),
 	packages=find_packages(
 		include="rinde/res",
-		exclude=["example_1", "example_2", "example_3"]
+		exclude=("example_1", "example_2", "example_3")
 	)
 )
